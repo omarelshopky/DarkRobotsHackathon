549e9930683bf04c2b3c6f65c16865e056f0d20f
Omar Hesham <oommaarr.220022@gmail.com>
2022-03-02 07:32:29 +0200

    Imporve compare times function

--- components/Timeline.py
+++ components/Timeline.py
@@ -1,4 +1,4 @@
-from globals import fileHandler, timeline
+from components.globals import fileHandler, timeline
 from datetime import datetime
 
 
@@ -8,16 +8,16 @@ class Timeline:
         now = datetime.now()
         timeNow = now.strftime(fileHandler.config["TIME_FORMATE"])
         
-        if now.strftime("%m/%d/%y") != fileHandler.config["HACKATHON_DATE"] or self.compare2time(timeNow, fileHandler.config["HACKATHON_END"], ">="):
+        if now.strftime("%m/%d/%y") != fileHandler.config["HACKATHON_DATE"] or self.compareWithCurrentTime(fileHandler.config["HACKATHON_END"], ">="):
             return ["Finished", '0:00']
         else:
             
-            if self.compare2time(timeNow, fileHandler.config["HACKATHON_START"], "<"):
+            if self.compareWithCurrentTime(fileHandler.config["HACKATHON_START"], "<"):
                 return ["Doesn't Start Yet", fileHandler.config["HACKATHON_START"][:-3]]
 
             else:
                 for event in list(timeline.keys())[::-1]:
-                    if self.compare2time(timeNow, timeline[event]["start"], '>='):
+                    if self.compareWithCurrentTime(timeline[event]["start"], '>='):
                         ret = [timeline[event]["name"]]
 
                         if event == list(timeline.keys())[-1]:
@@ -27,15 +27,17 @@ class Timeline:
 
                         return ret
 
-    def compare2time(self, stime1, stime2, sign):
+    def compareWithCurrentTime(self, stime, sign):
+        timeNow = datetime.now().strftime(fileHandler.config["TIME_FORMATE"])
+        
         if sign == '==':
-            return datetime.strptime(stime1, fileHandler.config['TIME_FORMATE']) == datetime.strptime(stime2, fileHandler.config['TIME_FORMATE'])
+            return datetime.strptime(timeNow, fileHandler.config['TIME_FORMATE']) == datetime.strptime(stime, fileHandler.config['TIME_FORMATE'])
         elif sign == '>':
-            return datetime.strptime(stime1, fileHandler.config['TIME_FORMATE']) > datetime.strptime(stime2, fileHandler.config['TIME_FORMATE'])
+            return datetime.strptime(timeNow, fileHandler.config['TIME_FORMATE']) > datetime.strptime(stime, fileHandler.config['TIME_FORMATE'])
         elif sign == '>=':
-            return datetime.strptime(stime1, fileHandler.config['TIME_FORMATE']) >= datetime.strptime(stime2, fileHandler.config['TIME_FORMATE'])
+            return datetime.strptime(timeNow, fileHandler.config['TIME_FORMATE']) >= datetime.strptime(stime, fileHandler.config['TIME_FORMATE'])
         elif sign == '<':
-            return datetime.strptime(stime1, fileHandler.config['TIME_FORMATE']) < datetime.strptime(stime2, fileHandler.config['TIME_FORMATE'])
+            return datetime.strptime(timeNow, fileHandler.config['TIME_FORMATE']) < datetime.strptime(stime, fileHandler.config['TIME_FORMATE'])
         elif sign == '<=':
-            return datetime.strptime(stime1, fileHandler.config['TIME_FORMATE']) <= datetime.strptime(stime2, fileHandler.config['TIME_FORMATE'])
+            return datetime.strptime(timeNow, fileHandler.config['TIME_FORMATE']) <= datetime.strptime(stime, fileHandler.config['TIME_FORMATE'])
 
