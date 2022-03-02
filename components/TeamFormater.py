14a34780087f6395a1b8e1f0ba1232aef936224a
Omar Hesham <oommaarr.220022@gmail.com>
2022-03-02 02:07:41 +0200

    Use fileHandler as global variable in Team Formater model

--- components/TeamFormater.py
+++ components/TeamFormater.py
@@ -1,21 +1,18 @@
-import FileHandler
 import random
-
+from globals import fileHandler
 class TeamFormater:
-    def __init__(self):
-        self.fileHandler = FileHandler()
 
     def increaseTeamCount(self, teams, team_number):
         """ Increases specific team count by one """
         teams[str(team_number)]["team-count"] += 1
-        self.fileHandler.updateTeams(teams)
+        fileHandler.updateTeams(teams)
 
     def assignToTeam(self):
         """ Assigns player to """
         # Load current teams data
-        teams = self.fileHandler.loadTeams()
+        teams = fileHandler.loadTeams()
         while True:
-            randTeam = random.randint(1, self.fileHandler.config["TEAMS_COUNT"])
-            if teams[str(randTeam)]["team-count"] != self.fileHandler.config["TEAMS_MAX"]:
+            randTeam = random.randint(1, fileHandler.config["TEAMS_COUNT"])
+            if teams[str(randTeam)]["team-count"] != fileHandler.config["TEAMS_MAX"]:
                 self.increaseTeamCount(teams, randTeam)
                 break
